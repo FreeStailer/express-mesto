88e8e3e9d58944eef80a3aeced721ea1b11dc247
Aleksey Suslin <aleksey-suslin@yandex.ru>
2021-04-20 19:37:14 +0300

    S15 corse4

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 });
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', 'no-cors');
   res.header('Access-Control-Allow-Headers', '*');
   res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS');
   if (req.method === 'OPTIONS') {
